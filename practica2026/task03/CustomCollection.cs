9b7675ad20f06b6b8cfd1f0594ee3118f7b4a392
Katerinn-s <ekaterinapolagina333@gmail.com>
2026-07-06 14:51:50 +0600

    06.07.2026 14:51

--- practica2026/task03/CustomCollection.cs
+++ practica2026/task03/CustomCollection.cs
@@ -36,8 +36,9 @@ namespace task03
 
         public IEnumerable<T> FilterAndSort(Func<T, bool> predicate, Func<T, IComparable> keySelector)
         {
+            if((predicate == null)||(keySelector == null))
+                return Enumerable.Empty<T>();
             return _items.Where(predicate).OrderBy(keySelector);
         }
-        //IEnumerator IEnumerable.GenerateSequence() => GetEnumerator();
     }
 }
