8dac2c2f868e47ed45c49fbb2a7b28c5afdf9d51
Katerinn-s <ekaterinapolagina333@gmail.com>
2026-07-10 14:42:37 +0600

    10.07.2026 14:42

--- practica2026/task11tests/UnitTest1.cs
+++ practica2026/task11tests/UnitTest1.cs
@@ -7,11 +7,19 @@ namespace task11tests
 {
     public class CalculatorTests
     {
-        [Fact]
-        public void Calculator_Add_ReturnsCorrectSum()
+       [Fact]
+        public void Calculator_Add()
         {
             var generator = new CalculatorGenerator();
-            dynamic calculator = generator.CreateCalculatorInstance();
+            string _calculatorCode =   "using System; " +
+        "public class Calculator : task11.ICalculator " +
+        "{ " +
+        "    public int Add(int a, int b) => a + b; " +
+        "    public int Minus(int a, int b) => a - b; " +
+        "    public int Mul(int a, int b) => a * b; " +
+        "    public int Div(int a, int b) => a / b; " +
+        "}";
+            dynamic calculator = generator.GenerateClass(_calculatorCode);
 
             int result = calculator.Add(5, 3);
             Assert.Equal(8, result);
