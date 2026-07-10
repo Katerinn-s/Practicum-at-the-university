54d9c7ffe7d843fff923ac79a679951abdf20713
Katerinn-s <ekaterinapolagina333@gmail.com>
2026-07-10 17:25:35 +0600

    10.07.2026 17:25

--- practica2026/task11tests/UnitTest1.cs
+++ practica2026/task11tests/UnitTest1.cs
@@ -22,5 +22,41 @@ namespace task11tests
             int result = calculator.Add(5, 3);
             Assert.Equal(8, result);
         }
+
+        [Fact]
+        public void Calculator_Minus_ReturnsCorrectDifference()
+        {
+            string _calculatorCode =
+                "using System; " +
+                "public class Calculator : task11.ICalculator " +
+                "{ " +
+                "    public int Add(int a, int b) => a + b; " +
+                "    public int Minus(int a, int b) => a - b; " +
+                "    public int Mul(int a, int b) => a * b; " +
+                "    public int Div(int a, int b) => a / b; " +
+                "}";
+            var generator = new CalculatorGenerator();
+            ICalculator calculator = generator.CreateCalculatorInstance(_calculatorCode);
+            int result = calculator.Minus(10, 4);
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void Calculator_Mul_ReturnsCorrectProduct()
+        {
+            string _calculatorCode =
+                "using System; " +
+                "public class Calculator : task11.ICalculator " +
+                "{ " +
+                "    public int Add(int a, int b) => a + b; " +
+                "    public int Minus(int a, int b) => a - b; " +
+                "    public int Mul(int a, int b) => a * b; " +
+                "    public int Div(int a, int b) => a / b; " +
+                "}";
+            var generator = new CalculatorGenerator();
+            ICalculator calculator = generator.CreateCalculatorInstance(_calculatorCode);
+            int result = calculator.Mul(6, 7);
+            Assert.Equal(42, result);
+        }
     }
 }
